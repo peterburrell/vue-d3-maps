fc7df1828778bb02a0125b49ea5996a06fd6c857
Peter Burrell <peterburrell@gmail.com>
2017-05-26 17:18:20 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <world-map :width="900" :height="600" :projection="projection"></world-map>
+    <br/>
+    <select v-model="projection">
+      <option v-for="p in projections" :value="p">
+        {{ p }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import WorldMap from './components/WorldMap.vue';
+
+export default {
+  name: 'app',
+  components: { WorldMap },
+  data () {
+    return {
+      projection: 'mercator',
+      projections: ['mercator', 'orthographic', 'stereographic', 'conic']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  body {
+    font-size: 14px;
+  }
+
+  select {
+    font-size: inherit;
+    padding: 5px;
+    border-color: #ddd;
+  }
+</style>
---
+++ src/components/WorldMap.vue
@@ -0,0 +1,99 @@
+<template>
+    <svg class="map" :width="width" :height="height">
+      <path class="land" :d="land" />
+      <path class="boundary" :d="countries" />
+      <path class="graticule" :d="graticule" />
+    </svg>
+</template>
+
+<script>
+import * as topojson from "topojson-client";
+import { geoMercator, geoOrthographic, geoStereographic, geoConicEqualArea, geoGraticule, geoPath } from "d3-geo";
+import world from 'world-atlas/world/110m.json';
+
+const land = topojson.feature(world, world.objects.land);
+const countries = topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; });
+const graticule = geoGraticule();
+
+// todo: move this into its own module
+const projections = {
+  'mercator': geoMercator,
+  'orthographic': geoOrthographic,
+  'stereographic': geoStereographic,
+  'conic': geoConicEqualArea
+};
+
+export default {
+  props: {
+    width: {
+      type: Number,
+      default: 1200
+    },
+    height: {
+      type: Number,
+      default: 800
+    },
+    projection: {
+      type: String,
+      default: 'mercator'
+    }
+  },
+  computed: {
+    _projection() {
+      return projections[this.projection]()
+        .scale((this.width - 3) / (2 * Math.PI))
+        .translate([this.width / 2, this.height / 2]);
+    },
+    graticule() {
+      const path = geoPath(this._projection);
+      return path(graticule);
+    },
+    land() {
+      const path = geoPath(this._projection);
+      return path(land);
+    },
+    countries() {
+      const path = geoPath(this._projection);
+      return path(countries);
+    }
+  },
+  created() {
+    console.log(this.graticule);
+  }
+}
+</script>
+
+<style lang="scss">
+.map {
+  /*background-color: #a3daff;*/
+}
+
+.stroke {
+  fill: none;
+  stroke: #000;
+  stroke-width: 3px;
+}
+
+.fill {
+  fill: #fff;
+}
+
+.graticule {
+  fill: none;
+  stroke: #777;
+  stroke-width: 0.5px;
+  stroke-opacity: 0.5;
+}
+
+.land {
+  /*fill: #1862ab;*/
+  fill: #444;
+}
+
+.boundary {
+  fill: none;
+  stroke: #fff;
+  stroke-width: 0.5px;
+}
+
+</style>
